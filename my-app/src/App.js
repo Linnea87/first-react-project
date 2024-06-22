a0cc6e44098332e15bd65ac558f81a3f1549f552
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-06-22 16:38:12 +0000

    StatefulGreetingWithPrevState lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 // import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
 // import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!"  name="Mike"  age="32" /> */}
-      <StatefulGreetingWithCallback/>
+      <StatefulGreetingWithPrevState/>
     </div>
   );
 }
