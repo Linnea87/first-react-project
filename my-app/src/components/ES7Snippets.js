de5cc14c87ecdf0fa41438beb699ee080f7db762
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-06-26 09:53:00 +0000

    ES7Snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,56 @@
+//rfce snippets - creates a functional component 
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rafce snippets - creates arrow functional component
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rce snippets - creates a class component
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// rconst snippets - adds a default constructor for a class that contains props arguments
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+     
+//   }
+// }
+
+// clg snippets - console.log(object)
+
+// console.log(object)
+
+// clo snippets - consolde.log('object', object)
+
+// console.log('object', object)
+
