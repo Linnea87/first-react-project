4261a2af5106eb8c3d8f595b8dfd20cdcf4b735b
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-07-12 15:45:18 +0000

    form lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,10 @@ import './App.css';
 // import RenderingLists from './components/RenderingLists';
 // import LifeCyclesCDM from './components/LifeCyclesCDM';
 // import LifeCyclesCDU from './components/LifeCyclesCDU';
-import LifeCyclesCWU from './components/LifeCyclesCWU';
+// import LifeCyclesCWU from './components/LifeCyclesCWU';
+// import ControlledForm from './components/ControlledForm';
+// import UncontrolledForm from './components/UncontrolledForm'
+import SearchBar from './components/SearchBar';
 
 
 function App() {
@@ -32,7 +35,10 @@ function App() {
       {/* <RenderingLists /> */}
       {/* <LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
-      <LifeCyclesCWU />
+      {/* <LifeCyclesCWU /> */}
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledForm /> */}
+      <SearchBar />
     </div>
   );
 }
