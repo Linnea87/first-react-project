1984ae4e830d0cd3c3047c09c25dadbc3f226196
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-07-13 15:37:22 +0000

    cleanup function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,7 +21,8 @@ import './App.css';
 // import ControlledFormHooks from './components/ControlledFormHooks';
 // import UseStateWithArrays from './components/UseStateWithArrays';
 // import UseStateWithObjects from './components/UseStateWithObjects';
-import UseEffectCounter from './components/UseEffectCounter';
+// import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 
 
 function App() {
@@ -46,7 +47,8 @@ function App() {
       {/* <ControlledFormHooks /> */}
       {/* <UseStateWithArrays /> */}
       {/* <UseStateWithObjects /> */}
-      <UseEffectCounter />
+      {/* <UseEffectCounter /> */}
+      <UseEffectCounterContainer />
     </div>
   );
 }
